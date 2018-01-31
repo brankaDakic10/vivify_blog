2c12378328780432dc0a8fb43437cfc3b70e414d
Branka Dakic <branka.dakic@yahoo.com>
2018-01-31 22:02:13 +0100

    Finished 4.task

--- partials/posts.php
+++ partials/posts.php
@@ -1,4 +1,93 @@
+<?php 
+// Zadatak4//
+$servername='127.0.0.1';
+$username='root';
+$password='vivify';
+$dbname='blog';
 
-<?php include 'partials/single-post1.php';?>
-<?php include 'partials/single-post2.php';?>
-<?php include 'partials/single-post3.php';?>
+ try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+
+
+
+
+
+// Zadatak4//
+    //9.Na posts.php stranici zameniti hard-kodovane postove i dovući postove iz baze. Postove poredjati po datumu, najnoviji post treba da se nalazi na vrhu.
+
+
+ // pripremamo upit
+                $sql = "SELECT Id, Title, Body, Author, Created_at FROM posts ORDER BY Created_at DESC";
+                $statement = $connection->prepare($sql);
+                // izvrsavamo upit
+                $statement->execute();
+                // zelimo da se rezultat vrati kao asocijativni niz.
+                // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+                $statement->setFetchMode(PDO::FETCH_ASSOC);
+                // punimo promenjivu sa rezultatom upita
+                $posts = $statement->fetchAll();
+                // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+                    // echo '<pre>';
+                    // var_dump($posts);
+                    // echo '</pre>';
+
+
+
+
+ ?>
+
+
+            <?php
+                foreach ($posts as $post) {
+            ?>
+             <div class="blog-post">
+
+                <h2 class="blog-post-title"><a href="/partials/single-post1.php"><?php echo($post['Title']) ?></a></h2>
+                <p class="blog-post-meta"><?php echo($post['Created_at']) ?> by <a href="#"><?php echo($post['Author']) ?></a></p>
+
+                <p><?php echo($post['Body']) ?></p>
+               <!--  <hr>
+                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+                <blockquote>
+                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                </blockquote>
+                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <h2>Heading</h2>
+                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                <h3>Sub-heading</h3>
+                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <pre><code>Example code block</code></pre>
+                <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+                <h3>Sub-heading</h3>
+                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <ul>
+                    <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
+                    <li>Donec id elit non mi porta gravida at eget metus.</li>
+                    <li>Nulla vitae elit libero, a pharetra augue.</li>
+                </ul>
+                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
+                <ol>
+                    <li>Vestibulum id ligula porta felis euismod semper.</li>
+                    <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
+                    <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
+                </ol>
+                <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p> -->
+            </div><!-- /.blog-post -->
+
+
+            <?php
+                }
+            ?>
+
+
+
+
+
+
